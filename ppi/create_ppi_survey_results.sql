223868f9010f9e4e6a72b14f68596110bfc1c8e0
Van Mittal-Henkle <van.mittalhenkle@gmail.com>
2010-10-07 16:05:42 -0700

    WIP on PPI, added score version column to ppi_survey_results table.

--- ppi/create_ppi_survey_results.sql
+++ ppi/create_ppi_survey_results.sql
@@ -1,9 +1,10 @@
 CREATE TABLE `ppi_survey_results` (
   `ppi_results_id` int(11) NOT NULL AUTO_INCREMENT,
-  `ppi_survey_name` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
-  `date_survey_taken` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
-  `client_id` int(11) DEFAULT NULL,
-  `ppi_score` int(11) DEFAULT NULL,
+  `ppi_survey_name` varchar(45) NOT NULL,
+  `points_version` int(11) NOT NULL,
+  `date_survey_taken` varchar(45) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `ppi_score` int(11) NOT NULL,
   PRIMARY KEY (`ppi_results_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
