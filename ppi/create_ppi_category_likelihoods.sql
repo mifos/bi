7037171af137739d52f56b094efd3470de6dd5a6
Van Mittal-Henkle <van.mittalhenkle@gmail.com>
2010-10-13 09:37:34 -0700

    WIP on PPI reports. Added PPI version information to tables.

--- ppi/create_ppi_category_likelihoods.sql
+++ ppi/create_ppi_category_likelihoods.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `ppi_category_likelihoods` (
   `ppi_category_likelihoods_id` int(11) NOT NULL AUTO_INCREMENT,
-  `ppi_survey` varchar(45) CHARACTER SET latin1 NOT NULL,
-  `ppi_measure` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `ppi_survey` varchar(45) NOT NULL,
+  `likelihoods_version` int(11) NOT NULL,
+  `ppi_measure` varchar(45) NOT NULL,
   `lower_bound` int(11) NOT NULL,
   `upper_bound` int(11) NOT NULL,
   `likelihood` decimal(4,1) NOT NULL,
--- ppi/list_clients_in_date_range_with_ppi.sql
+++ ppi/list_clients_in_date_range_with_ppi.sql
@@ -1,4 +1,4 @@
-select * from mifos_ppi_dw.dim_customer dc, mifos_ppi_dw.ppi_survey_results ppi
+select count(*) from dim_customer dc, ppi_survey_results ppi
 where dc.customer_level_id = 1 
 and dc.customer_status = 'ClientStatus-ApplicationPendingApproval'
 and ppi.client_id = dc.customer_id
