4dea72ff7ced266865bed2b79a3b5c810c9195ec
Van Mittal-Henkle <van.mittalhenkle@gmail.com>
2010-10-29 15:39:58 -0700

    Updating colummns in table ppi_category_likelihood_bands and updating plines_parser.py script to generate category likelihood sql. Updating colummns in table ppi_category_likelihood_bands to support PPI for all countries. Temporarily removed preloaded data for India and Kenya. Updated plines_parser.py to map to new columns and adjusted mappings for PPP columns with no 2005 or 1993 specified.

--- ppiparser/plines_parser.py
+++ ppiparser/plines_parser.py
@@ -15,15 +15,15 @@ COLUMN_MAP = {
 'Total Below $1/Day/PPP': 'below_1_00_per_day_1993_ppp',
 'Total Below 150% of National Poverty Line': 'below_150_percent_of_national',
 'Total Below 200% of National Poverty Line': 'below_200_percent_of_national',
-'Total Below Lower National Food Poverty Line': 'below_national_food', # !!! no column "below_loer_national_food"
+'Total Below Lower National Food Poverty Line': 'below_lower_national_food', # !!! no column "below_loer_national_food" *fixed*
 'Total Below National Poverty Line': 'below_national',
 'Total Below Upper National Poverty Line': 'below_upper_national',
-'Total Below the $0.25/Day/PPP Line': 'below_0_25_per_day_2005_ppp',
-'Total Below the $0.50/Day/PPP Line': 'below_0_50_per_day_2005_ppp',
-'Total Below the $0.75/Day/PPP Line': 'below_0_75_per_day_2005_ppp',
+'Total Below the $0.25/Day/PPP Line': 'below_0_25_per_day_1993_ppp',
+'Total Below the $0.50/Day/PPP Line': 'below_0_50_per_day_1993_ppp',
+'Total Below the $0.75/Day/PPP Line': 'below_0_75_per_day_1993_ppp',
 'Total Below the $1.08/Day/1993 PPP Line': 'below_1_08_per_day_1993_ppp',
 'Total Below the $1.25/Day/2005 PPP Line': 'below_1_25_per_day_2005_ppp',
-'Total Below the $1.25/Day/PPP Line': 'below_1_25_per_day_2005_ppp', # !!! uses 2005 like above
+'Total Below the $1.25/Day/PPP Line': 'below_1_25_per_day_1993_ppp', # !!! uses 2005 like above *fixed*
 'Total Below the $1.50/Day/PPP Line': 'below_1_50_per_day_2005_ppp',
 'Total Below the $1.75/Day/2005 PPP Line': 'below_1_75_per_day_2005_ppp',
 'Total Below the $1/Day/1993 PPP Line': 'below_1_00_per_day_1993_ppp',
@@ -31,31 +31,31 @@ COLUMN_MAP = {
 'Total Below the $1/Day/PPP Line': 'below_1_00_per_day_1993_ppp',
 'Total Below the $2.16/Day/1993 PPP Line': 'below_2_16_per_day_1993_ppp',
 'Total Below the $2.50/Day/2005 PPP Line': 'below_2_50_per_day_2005_ppp',
-'Total Below the $2.50/Day/PPP Line': 'below_2_50_per_day_2005_ppp', # !!! uses 2005 like above
+'Total Below the $2.50/Day/PPP Line': 'below_2_50_per_day_1993_ppp', # !!! uses 2005 like above *fixed*
 'Total Below the $2/Day/1993 PPP Line': 'below_2_00_per_day_1993_ppp',
-'Total Below the $2/Day/PPP Line': 'below_2_00_per_day_1993_ppp', # !!! uses 1993 like above
-'Total Below the $3.00/Day/2005 PPP Line': None, # !!! no column for $3.00
+'Total Below the $2/Day/PPP Line': 'below_2_00_per_day_1993_ppp', # !!! uses 1993 like above *fixed*
+'Total Below the $3.00/Day/2005 PPP Line': 'below_3_00_per_day_2005_ppp', # !!! no column for $3.00 *fixed*
 'Total Below the $3.24/Day/1993 PPP Line': 'below_3_24_per_day_1993_ppp',
 'Total Below the $3.75/Day/2005 PPP Line': 'below_3_75_per_day_2005_ppp',
-'Total Below the $3.75/Day/PPP Line': 'below_3_75_per_day_2005_ppp', # !!! uses 2005 like above
+'Total Below the $3.75/Day/PPP Line': 'below_3_75_per_day_1993_ppp', # !!! uses 2005 like above *fixed*
 'Total Below the $4.00/Day/2005 PPP Line': 'below_4_00_per_day_2005_ppp',
 'Total Below the $4.32/Day/1993 PPP Line': 'below_4_32_per_day_1993_ppp',
 'Total Below the $4/Day/2005 PPP Line': 'below_4_00_per_day_2005_ppp',
 'Total Below the $5.00/Day/2005 PPP Line': 'below_5_00_per_day_2005_ppp',
 'Total Below the $5/Day/2005 PPP Line': 'below_5_00_per_day_2005_ppp',
-'Total Below the $5/Day/PPP Line': 'below_5_00_per_day_2005_ppp', # !!! 3 above use 2005
+'Total Below the $5/Day/PPP Line': 'below_5_00_per_day_1993_ppp', # !!! 3 above use 2005 *fixed*
 'Total Below the $6.25/Day/2005 PPP Line': 'below_6_25_per_day_2005_ppp',
-'Total Below the 100% National Poverty Line': None,
-'Total Below the 100% of National Poverty Line': None, # !!! no column below_100_percent_of_national
+'Total Below the 100% National Poverty Line': 'below_national',
+'Total Below the 100% of National Poverty Line': 'below_national', # !!! no column below_100_percent_of_national *fixed*
 'Total Below the 125% National Poverty Line': 'below_125_percent_of_national',
 'Total Below the 125% of National Asset Poverty Line': 'below_125_percent_of_national_asset',
 'Total Below the 125% of the National Poverty Line': 'below_125_percent_of_national_asset',
 'Total Below the 150% of National Asset Poverty Line': 'below_150_percent_of_national_asset',
 'Total Below the 150% of the National Poverty Line': 'below_150_percent_of_national',
-'Total Below the 150% of the Upper National Poverty Line': None, # !!! no column,
+'Total Below the 150% of the Upper National Poverty Line': 'below_150_percent_of_upper_national_asset', # !!! no column, *fixed*
 'Total Below the 200% National Poverty Line': 'below_200_percent_of_national',
 'Total Below the 200% of the National Poverty Line': 'below_200_percent_of_national',
-'Total Below the 200% of the Upper National Poverty Line': None, # !!! no column
+'Total Below the 200% of the Upper National Poverty Line': 'below_200_percent_of_upper_national_asset', # !!! no column *fixed*
 'Total Below the 300% of the National Poverty Line': 'below_300_percent_of_national',
 'Total Below the 50% National Poverty Line': 'below_50_percent_of_national',
 'Total Below the 75% National Poverty Line': 'below_75_percent_of_national',
@@ -74,13 +74,13 @@ COLUMN_MAP = {
 'Total Below the Subsistence Poverty Line': 'below_subsistence',
 'Total Below the Two-Minimum-Wage Line': 'below_two_minimum_wage',
 'Total Below the USAID Extreme Poverty Line': 'below_usaid_extreme',
-'Total Below the Upper Line': 'below_upper_',
+'Total Below the Upper Line': 'below_upper',
 'Total Below the Upper National Poverty Line': 'below_upper_national',
 
 }
 
 # For India: columns which have *2005* version instead of original *1993*
-COLUMNS_2005_VERSION = ('below_1_00_per_day_1993_ppp', 'below_2_00_per_day_1993_ppp')
+COLUMNS_2005_VERSION = ('below_0_75_per_day_1993_ppp', 'below_1_00_per_day_1993_ppp', 'below_1_25_per_day_1993_ppp', 'below_2_00_per_day_1993_ppp')
 
 
 INSERT_TEMPLATE = 'insert into ppi_category_likelihood_bands ({COLUMNS}) values ({VALUES});'
