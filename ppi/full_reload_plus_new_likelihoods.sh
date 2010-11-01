f3a29aff74ddddcff7d80439a0a824099a5cb335
Adam Monsen <haircut@gmail.com>
2010-11-01 12:20:09 -0700

    refactor to make this work like full_reload.sh
    This script is just for testing.
    
    use Romania for now (that's what we're currently testing)

--- ppi/full_reload_plus_new_likelihoods.sh
+++ ppi/full_reload_plus_new_likelihoods.sh
@@ -2,21 +2,21 @@
 #
 #drop and recreate the DW and then recreate and reload PPI tables
 #
-#usage: full_reload <DB_NAME> <PENTAHO_HOME> <BI_HOME>
-#example: full_reload mifos_ppi_dw ~/pentaho ~/reportingWorkspace/bi
+#usage: full_reload <DB_NAME> <PDI_HOME> <BI_HOME>
+#example: full_reload mifos_ppi_dw ~/pentaho-data-integration ~/reportingWorkspace/bi
 
 set -o errexit
 DB_NAME=$1
-PENTAHO_HOME=$2
-PDI_HOME=${PENTAHO_HOME}/data-integration
+PDI_HOME=$2
 BI_HOME=$3
+MYSQL_ARGS="-u root"
 echo $DB_NAME
-echo "drop database ${DB_NAME}" | mysql -u root
-echo "create database ${DB_NAME}" | mysql -u root
-mysql -u root ${DB_NAME} < ../JohnWoodlockWorkInProgress/MifosDataWarehouseETL/load_mifos_datawarehouse.sql
+echo "drop database $DB_NAME" | mysql $MYSQL_ARGS
+echo "create database $DB_NAME" | mysql $MYSQL_ARGS
+mysql $MYSQL_ARGS ${DB_NAME} < ${BI_HOME}/JohnWoodlockWorkInProgress/MifosDataWarehouseETL/load_mifos_datawarehouse.sql
 
 # while there is work in progress, load new category likelihoods here
-mysql -u root ${DB_NAME} < categoryLikelihoodsIndia02.sql
+mysql $MYSQL_ARGS ${DB_NAME} < /tmp/RomaniaPovertyLines.sql
 
 
 ${PDI_HOME}/kitchen.sh /file:${BI_HOME}/JohnWoodlockWorkInProgress/MifosDataWarehouseETL/CreateDataWarehouse.kjb
