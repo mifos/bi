1c85226eb7cb30ca4bb7fa137b14994351313d6d
Adam Monsen <haircut@gmail.com>
2010-11-02 15:12:50 -0700

    Similar to last commit. BI_HOME arg no longer needed.

--- JohnWoodlockWorkInProgress/MifosDataWarehouseETL/full_reload.sh
+++ JohnWoodlockWorkInProgress/MifosDataWarehouseETL/full_reload.sh
@@ -2,15 +2,32 @@
 #
 #drop and recreate the DW and then recreate and reload PPI tables
 #
-#usage: full_reload <DB_NAME> <PDI_HOME> <BI_HOME>
 #example: full_reload mifos_ppi_dw ~/pentaho-data-integration ~/reportingWorkspace/bi
+
+if [ $# -lt 2 ]
+then
+    echo "Usage: `basename $0` <DB_NAME> <PDI_HOME>"
+    exit 1
+fi
+
+PRG="$0"
+
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+PRGDIR=`dirname "$PRG"`
 
 DB_NAME=$1
 PDI_HOME=$2
-BI_HOME=$3
 MYSQL_ARGS="-u root"
-echo $DB_NAME
 echo "drop database $DB_NAME" | mysql $MYSQL_ARGS
 echo "create database $DB_NAME" | mysql $MYSQL_ARGS
-mysql $MYSQL_ARGS $DB_NAME < $BI_HOME/JohnWoodlockWorkInProgress/MifosDataWarehouseETL/load_mifos_datawarehouse.sql
-$PDI_HOME/kitchen.sh /file:$BI_HOME/JohnWoodlockWorkInProgress/MifosDataWarehouseETL/CreateDataWarehouse.kjb
+mysql $MYSQL_ARGS $DB_NAME < $PRGDIR/load_mifos_datawarehouse.sql
+$PDI_HOME/kitchen.sh /file:`readlink -f $PRGDIR/CreateDataWarehouse.kjb`
