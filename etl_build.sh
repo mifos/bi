ad4928e4bccdb3dd92cbe53939ad2c4d151b2088
Adam Monsen <haircut@gmail.com>
2010-11-02 13:52:45 -0700

    etl_build: Automatically derive working directory.
    WORKSPACE environment variable no longer needed.

--- etl_build.sh
+++ etl_build.sh
@@ -7,25 +7,36 @@ DW_DB_NAME=$2
 PDI_HOME=$3
 MYSQL_ARGS=$4
 
-# set by Hudson
-BI_HOME=$WORKSPACE
+PRG="$0"
+
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+PRGDIR=`dirname "$PRG"`
 
 echo "Initializing OLTP database ($OLTP_DB_NAME)..."
 echo "drop database $OLTP_DB_NAME" | mysql $MYSQL_ARGS
 echo "create database $OLTP_DB_NAME" | mysql $MYSQL_ARGS
-mysql $MYSQL_ARGS $OLTP_DB_NAME < $BI_HOME/JohnWoodlockWorkInProgress/MifosDataWarehouseETLTest/mifos_testetl_db.sql
+mysql $MYSQL_ARGS $OLTP_DB_NAME < $PRGDIR/JohnWoodlockWorkInProgress/MifosDataWarehouseETLTest/mifos_testetl_db.sql
 
 echo "Initializing data warehouse ($DW_DB_NAME)..."
 echo "drop database $DW_DB_NAME" | mysql $MYSQL_ARGS
 echo "create database $DW_DB_NAME" | mysql $MYSQL_ARGS
-mysql $MYSQL_ARGS $DW_DB_NAME < $BI_HOME/JohnWoodlockWorkInProgress/MifosDataWarehouseETL/load_mifos_datawarehouse.sql
+mysql $MYSQL_ARGS $DW_DB_NAME < $PRGDIR/JohnWoodlockWorkInProgress/MifosDataWarehouseETL/load_mifos_datawarehouse.sql
 
 echo "Running ETL..."
 log=`mktemp`
-$PDI_HOME/kitchen.sh /file:$BI_HOME/JohnWoodlockWorkInProgress/MifosDataWarehouseETL/CreateDataWarehouse.kjb | tee $log
+$PDI_HOME/kitchen.sh /file:$PRGDIR/JohnWoodlockWorkInProgress/MifosDataWarehouseETL/CreateDataWarehouse.kjb | tee $log
 
 echo "Running tests..."
-$PDI_HOME/kitchen.sh /file:$BI_HOME/JohnWoodlockWorkInProgress/MifosDataWarehouseETLTest/TestDataWarehouseETL.kjb | tee -a $log
+$PDI_HOME/kitchen.sh /file:$PRGDIR/JohnWoodlockWorkInProgress/MifosDataWarehouseETLTest/TestDataWarehouseETL.kjb | tee -a $log
 
 exitcode=0
 if grep -q '^ERROR ' $log
