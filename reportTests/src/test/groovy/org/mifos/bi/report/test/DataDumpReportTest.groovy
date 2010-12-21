2f7ace334ce15e657f5545e8faee6ee6d57aa34c
Adam Monsen <haircut@gmail.com>
2010-12-21 12:55:17 -0800

    group/version were just for presto, removed.
    Also
    * removed unnecessary imports from DataDumpReportTest
    * PrptReport moved to the org.mifos.bi.test package

--- reportTests/src/test/groovy/org/mifos/bi/report/test/DataDumpReportTest.groovy
+++ reportTests/src/test/groovy/org/mifos/bi/report/test/DataDumpReportTest.groovy
@@ -1,8 +1,7 @@
 package org.mifos.bi.report.test
 
-import static org.junit.Assert.*
 import org.junit.Test
-import org.mifos.bi.PrptReport
+import org.mifos.bi.test.PrptReport
 
 class DataDumpReportTest {
     @Test
