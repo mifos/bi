1e6786b6783464d519b94a0b28676a39d64483c4
Jakub Slawinski <jeremian@poczta.fm>
2011-01-16 00:05:41 +0100

    MIFOSBI-182: Updated report according to the newest requirements.

--- reportTests/src/test/groovy/org/mifos/bi/test/LoanClassificationReportbyProductTest.groovy
+++ reportTests/src/test/groovy/org/mifos/bi/test/LoanClassificationReportbyProductTest.groovy
@@ -4,21 +4,22 @@ import org.junit.Test
 
 class LoanClassificationReportbyProductTest {
 
+    def mfiName = 'Mifos HO'
     def reportPath = 'reports/standardReports/prpts/LoanClassificationProduct.prpt'
     def reportName = 'Loan Classification by Product'
-    def detailsHeader = ['Product Name', '# of Loans Disbursed (during period)', 'Amount Disbursed (during period)',
-            'Amount Outstanding (end of period)', 'Arrears (end of period)', 'PAR (end of period)',
-            '# of Loans Disbursed (end of period)', 'Amount of Loans Disbursed (end of period)',
-            '# of Active Loans (end of period)', '% of Portfolio (end of period)']
+    def detailsHeader1 = [' ', 'During Period', 'Cumulative', 'End of Period']
+    def detailsHeader2 = ['Product Name', '# of Loans Disbursed', 'Amount Disbursed',
+            '# of Loans Disbursed', 'Amount Disbursed',
+            'Amount Outstanding', 'Arrears Amount', 'PAR',
+            '# of Active Loans', '% of Portfolio Amount']
 
-    def office = ['Office:', 'br2', 'MFI Name:', 'Mifos HO']
-    def byProductType = ['By Product Type']
-    def detailsbr2_grouploan = ['group loan', '0', '0.0000', '0.0000', '0.0000', '0', '0.0000', '0', '0.00']
-    def detailsbr2_loanprod1_20100815 = ['loan prod 1', '1', '3000.0000', '2924.9000', '781.1000', '100.00', '1', '3000.0000', '1', '100.00']
-    def detailsbr2_loanprod1_20100930 = ['loan prod 1', '1', '3000.0000', '2924.9000', '2924.9000', '100.00', '1', '3000.0000', '1', '100.00']
-    def detailsbr2_loanprod2 = ['loan prod 2', '0', '0.0000', '0.0000', '0.0000', '0', '0.0000', '0', '0.00']
-    def totalbr2_20100815 = ['Total', '1', '3000.0000', '2924.9000', '781.1000', '100.00000000000000', '1', '3000.0000', '1', '100.00']
-    def totalbr2_20100930 = ['Total', '1', '3000.0000', '2924.9000', '2924.9000', '100.00000000000000', '1', '3000.0000', '1', '100.00']
+    def office = ['Office:', 'br2']
+    def detailsbr2_grouploan = ['group loan', '0', '0.0000', '0', '0.0000', '0.0000', '0.0000', '0', '0.00']
+    def detailsbr2_loanprod1_20100815 = ['loan prod 1', '1', '3000.0000', '1', '3000.0000', '2924.9000', '781.1000', '100.00', '1', '100.00']
+    def detailsbr2_loanprod1_20100930 = ['loan prod 1', '1', '3000.0000', '1', '3000.0000', '2924.9000', '2924.9000', '100.00', '1', '100.00']
+    def detailsbr2_loanprod2 = ['loan prod 2', '0', '0.0000', '0', '0.0000', '0.0000', '0.0000', '0', '0.00']
+    def totalbr2_20100815 = ['Total', '1', '3000.0000', '1', '3000.0000', '2924.9000', '781.1000', '100.00000000000000', '1', '100.00']
+    def totalbr2_20100930 = ['Total', '1', '3000.0000', '1', '3000.0000', '2924.9000', '2924.9000', '100.00000000000000', '1', '100.00']
 
     def pageFooter = ['Version 1.0', 'Page', '1 / 1']
 
@@ -30,23 +31,22 @@ class LoanClassificationReportbyProductTest {
             t.reportPath = reportPath
             t.reportParams = ['selected_office': '1.1.1.', 'start_date': '2010-07-01', 'end_date': '2010-09-30']
 
+            // MFI Name
+            t.assertRowEquals(1, [mfiName])
             // Report title
-            t.assertRowEquals(1, [reportName])
+            t.assertRowEquals(2, [reportName])
 
             // Page header
-            t.assertRowEquals(2, office)
-            t.assertCellEquals(3, 1, 'Date Range:')
-            t.assertCellEquals(3, 2, 'From:')
-            // TODO t.assertCellEquals(3, 3, '2010-07-01')
-            t.assertCellEquals(3, 3, 'Report Date:')
+            t.assertCellEquals(3, 1, 'From:')
+            // TODO t.assertCellEquals(3, 2, '2010-07-01')
             t.assertCellEquals(4, 1, 'To:')
             // TODO t.assertCellEquals(4, 2, '2010-09-30')
+            t.assertRowEquals(5, office)
 
-            // Group header
-            def offset = 5
-            t.assertRowEquals(offset, byProductType)
             // Details header
-            t.assertRowEquals(offset+1, detailsHeader)
+            def offset = 6
+            t.assertRowEquals(offset, detailsHeader1)
+            t.assertRowEquals(offset+1, detailsHeader2)
             // group loan
             t.assertRowEquals(offset+2, detailsbr2_grouploan)
             // loan prod 1
@@ -57,9 +57,10 @@ class LoanClassificationReportbyProductTest {
             t.assertRowEquals(offset+5, totalbr2_20100930)
 
             // Page footer
-            offset = 11
+            offset = 12
             t.assertRowEquals(offset, pageFooter)
             t.assertCellEquals(offset+1, 1, 'Printed by:')
+            t.assertCellEquals(offset+1, 2, 'On:')
         }
     }
 
@@ -71,23 +72,22 @@ class LoanClassificationReportbyProductTest {
             t.reportPath = reportPath
             t.reportParams = ['selected_office': '1.1.1.', 'start_date': '2010-07-01', 'end_date': '2010-08-15']
 
+            // MFI Name
+            t.assertRowEquals(1, [mfiName])
             // Report title
-            t.assertRowEquals(1, [reportName])
+            t.assertRowEquals(2, [reportName])
 
             // Page header
-            t.assertRowEquals(2, office)
-            t.assertCellEquals(3, 1, 'Date Range:')
-            t.assertCellEquals(3, 2, 'From:')
-            // TODO t.assertCellEquals(3, 3, '2010-07-01')
-            t.assertCellEquals(3, 3, 'Report Date:')
+            t.assertCellEquals(3, 1, 'From:')
+            // TODO t.assertCellEquals(3, 2, '2010-07-01')
             t.assertCellEquals(4, 1, 'To:')
-            // TODO t.assertCellEquals(4, 2, '2010-09-30')
+            // TODO t.assertCellEquals(4, 2, '2010-08-15')
+            t.assertRowEquals(5, office)
 
-            // Group header
-            def offset = 5
-            t.assertRowEquals(offset, byProductType)
             // Details header
-            t.assertRowEquals(offset+1, detailsHeader)
+            def offset = 6
+            t.assertRowEquals(offset, detailsHeader1)
+            t.assertRowEquals(offset+1, detailsHeader2)
             // group loan
             t.assertRowEquals(offset+2, detailsbr2_grouploan)
             // loan prod 1
@@ -98,9 +98,10 @@ class LoanClassificationReportbyProductTest {
             t.assertRowEquals(offset+5, totalbr2_20100815)
 
             // Page footer
-            offset = 11
+            offset = 12
             t.assertRowEquals(offset, pageFooter)
             t.assertCellEquals(offset+1, 1, 'Printed by:')
+            t.assertCellEquals(offset+1, 2, 'On:')
         }
     }
 }
