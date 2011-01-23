db768b30be7c5707ec5705c3a1af93751f3b7373
Jakub Slawinski <jeremian@poczta.fm>
2011-01-23 13:08:39 +0100

    MIFOSBI-130: Fixed test.

--- reportTests/src/test/groovy/org/mifos/bi/test/ClientExitTest.groovy
+++ reportTests/src/test/groovy/org/mifos/bi/test/ClientExitTest.groovy
@@ -4,7 +4,9 @@ import static org.junit.Assert.*
 import org.junit.Test
 
 class ClientExitTest {
-    def reportName = 'Client Exit Report'
+
+    def mfiName = 'Mifos HO'
+    def reportName = 'Client Exit'
 
     @Test
     void testReport() {
@@ -12,8 +14,10 @@ class ClientExitTest {
             t.reportPath = 'reports/standardReports/prpts/ClientExit.prpt'
             t.reportParams = ['selected_office': '4', 'selected_loan_officer': '-1', 'start_date': '2010-07-01', 'end_date': '2010-08-15']
 
+            // MFI Name
+            t.assertRowEquals(1, [mfiName])
             // Report title
-            t.assertRowEquals(1, [reportName])
+            t.assertRowEquals(2, [reportName])
         }
     }
 }
