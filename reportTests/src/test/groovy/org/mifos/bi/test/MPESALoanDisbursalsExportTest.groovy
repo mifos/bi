1fa846494e1394bec02b1d88fccb728ff7fa6ed8
Rafal Lewandowski <rlewandowski@soldevelo.com>
2011-01-24 16:49:40 +0100

    MIFOS-4529: Created presto test for the report.

---
+++ reportTests/src/test/groovy/org/mifos/bi/test/MPESALoanDisbursalsExportTest.groovy
@@ -0,0 +1,58 @@
+package org.mifos.bi.test
+
+import org.junit.Test
+
+class MPESALoanDisbursalsExportTest {
+
+    def reportPath = 'reports/mpesa/MPESALoanDisbursalsExport.prpt'
+    def reportHeader = ['Amount', 'CreditIdentityString', 'CreditIdentityStringType', 'ValidationIdentityString', 'ValidationIdentityStringType']
+
+    @Test
+    void testParams_20100708() {
+        new PrptReport().execute () { t ->
+
+            // Report settings.
+            t.reportPath = reportPath
+            t.reportParams = ['extract_date': '2010-07-08']
+
+            // Report header
+            t.assertRowEquals(1, reportHeader)
+
+            // Details
+            t.assertRowEquals(2, ['6000.0000'])
+            t.assertRowEquals(3, ['3000.0000'])
+            t.assertRowEquals(4, ['6000.0000'])
+        }
+    }
+
+    @Test
+    void testParams_20100724() {
+        new PrptReport().execute () { t ->
+            
+            // Report settings.
+            t.reportPath = reportPath
+            t.reportParams = ['extract_date': '2010-07-24']
+
+            // Report header
+            t.assertRowEquals(1, reportHeader)
+
+            // Details
+            t.assertRowEquals(2, ['6000.0000'])
+        }
+    }
+
+    @Test
+    void testParams_20100727() {
+        new PrptReport().execute () { t ->
+
+            // Report settings.
+            t.reportPath = reportPath
+            t.reportParams = ['extract_date': '2010-07-27']
+
+            // Report header
+            t.assertRowEquals(1, reportHeader)
+        }
+    }
+
+}
+
