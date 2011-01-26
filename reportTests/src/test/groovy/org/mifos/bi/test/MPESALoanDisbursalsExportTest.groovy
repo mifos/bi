cf6ffe78ada885eb38138936d2603ca7563612df
Rafal Lewandowski <rlewandowski@soldevelo.com>
2011-01-26 12:30:05 +0100

    MIFOS-4529: Added the possibility to select groups for extract.

--- reportTests/src/test/groovy/org/mifos/bi/test/MPESALoanDisbursalsExportTest.groovy
+++ reportTests/src/test/groovy/org/mifos/bi/test/MPESALoanDisbursalsExportTest.groovy
@@ -8,12 +8,12 @@ class MPESALoanDisbursalsExportTest {
     def reportHeader = ['Amount', 'CreditIdentityString', 'CreditIdentityStringType', 'ValidationIdentityString', 'ValidationIdentityStringType']
 
     @Test
-    void testParams_20100708() {
+    void testParams_ALL_20100708() {
         new PrptReport().execute () { t ->
 
             // Report settings.
             t.reportPath = reportPath
-            t.reportParams = ['extract_date': '2010-07-08']
+            t.reportParams = ['c_group': '-1','extract_date': '2010-07-08']
 
             // Report header
             t.assertRowEquals(1, reportHeader)
@@ -29,12 +29,32 @@ class MPESALoanDisbursalsExportTest {
     }
 
     @Test
-    void testParams_20100724() {
+    void testParams_groupdw12_20100708() {
         new PrptReport().execute () { t ->
-            
+
             // Report settings.
             t.reportPath = reportPath
-            t.reportParams = ['extract_date': '2010-07-24']
+            t.reportParams = ['c_group': '5','extract_date': '2010-07-08']
+
+            // Report header
+            t.assertRowEquals(1, reportHeader)
+
+            // Details
+            t.assertRowEquals(2, ['3000.0000', '254321654987'])
+            t.assertRowEquals(3, ['6000.0000', 'ERROR'])
+
+            // Total number of rows
+            t.assertAllRowsNumber(3)
+        }
+    }
+
+    @Test
+    void testParams_groupdw11_20100708() {
+        new PrptReport().execute () { t ->
+
+            // Report settings.
+            t.reportPath = reportPath
+            t.reportParams = ['c_group': '2','extract_date': '2010-07-08']
 
             // Report header
             t.assertRowEquals(1, reportHeader)
@@ -48,12 +68,47 @@ class MPESALoanDisbursalsExportTest {
     }
 
     @Test
-    void testParams_20100727() {
+    void testParams_ALL_20100724() {
+        new PrptReport().execute () { t ->
+            
+            // Report settings.
+            t.reportPath = reportPath
+            t.reportParams = ['c_group': '-1', 'extract_date': '2010-07-24']
+
+            // Report header
+            t.assertRowEquals(1, reportHeader)
+
+            // Details
+            t.assertRowEquals(2, ['6000.0000', 'ERROR'])
+
+            // Total number of rows
+            t.assertAllRowsNumber(2)
+        }
+    }
+
+     @Test
+    void testParams_groupdw13_20100724() {
+        new PrptReport().execute () { t ->
+            
+            // Report settings.
+            t.reportPath = reportPath
+            t.reportParams = ['c_group': '8', 'extract_date': '2010-07-24']
+
+            // Report header
+            t.assertRowEquals(1, reportHeader)
+
+            // Total number of rows
+            t.assertAllRowsNumber(1)
+        }
+    }
+
+    @Test
+    void testParams_ALL_20100727() {
         new PrptReport().execute () { t ->
 
             // Report settings.
             t.reportPath = reportPath
-            t.reportParams = ['extract_date': '2010-07-27']
+            t.reportParams = ['c_group': '-1', 'extract_date': '2010-07-27']
 
             // Report header
             t.assertRowEquals(1, reportHeader)
