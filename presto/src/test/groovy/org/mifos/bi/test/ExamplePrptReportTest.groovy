cedeb5b7b142652efdf21026c75ad1a5daf1e400
Jakub Slawinski <jeremian@poczta.fm>
2011-02-06 20:03:17 +0100

    Fixed tests after recent changes to dw database.

--- presto/src/test/groovy/org/mifos/bi/test/ExamplePrptReportTest.groovy
+++ presto/src/test/groovy/org/mifos/bi/test/ExamplePrptReportTest.groovy
@@ -13,7 +13,9 @@ class ExamplePrptReportTest {
             // Output data tests. NOTE: use 1-based indices!
             t.assertCellEquals(1, 1, "Customer ID")
             t.assertCellEquals(1, 2, "Customer Status")
-            t.assertRowEquals(2, ['1'])
+            t.assertRowEquals(2, ['0'])
+            t.assertRowEquals(3, ['1'])
+            t.assertRowEquals(4, ['2'])
 
             t.assertRowsNumber(4)
             t.assertAllRowsNumber(4)
