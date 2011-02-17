565e1be68aa83ec25ac67cb87551b6cc812b897d
Adam Balcerzak <abalcerzak@soldevelo.com>
2011-02-17 14:01:07 +0100

    corrected Interest Rate for all ActiveLoans* reports

--- reportTests/src/test/groovy/org/mifos/bi/test/ActiveLoansLoanPurpose.groovy
+++ reportTests/src/test/groovy/org/mifos/bi/test/ActiveLoansLoanPurpose.groovy
@@ -9,8 +9,8 @@ class ActiveLoansLoanPurposeTest {
     def detailsHeader = ['System Id', 'Center Name', 'Group Name', 'Client Name', 'Loan Amount', 'Interest Rate', 'Disbursal Date', 'Repaid Principal', 'Repaid Interest', 'Outstanding Principal', 'Outstanding Interest', 'Loan Officer']
 
     def center = ['Loan Purpose:', 'n/a'] 
-    def details = ['32', 'br2 center 1', 'br2 group 1', 'br2 client 1 xxx', '3000.0000', '21.0000', '2010-07-21', '75.1000', '2.9000', '2924.9000', '18.1000', 'br2 LO xxx']
-    def total = ['Total', '', '', '', '3000.0000', '21.0000', '', '75.1000', '2.9000', '2924.9000', '18.1000', '']
+    def details = ['32', 'br2 center 1', 'br2 group 1', 'br2 client 1 xxx', '3000.0000', '5.0000000000', '2010-07-21', '75.1000', '2.9000', '2924.9000', '18.1000', 'br2 LO xxx']
+    def total = ['Total', '', '', '', '3000.0000', '', '', '75.1000', '2.9000', '2924.9000', '18.1000', '']
 
     def pageFooter = ['Version 1.2', 'Page', '1 / 1']
 
