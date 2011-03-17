b865ba88ea5c69ca801244beca78acca01c3f401
Vishwas Babu A J <vishwas@confluxtechnologies.com>
2011-03-17 23:46:41 +0530

    Modified Closed loans and Overdue Mature loans Report

---
+++ reportTests/src/test/groovy/org/mifos/bi/test/ClosedLoans.groovy
@@ -0,0 +1,215 @@
+package org.mifos.bi.test
+
+import org.junit.Test
+
+class ClosedLoans {
+
+
+  def reportPath = 'reports/standardReports/prpts/ClosedLoans.prpt'
+  def reportName = 'Closed Loans'
+
+  /** Headers for Closed Loans**/
+  
+  def closedLoansHeader = ['Group Name','Member Name','Loan Id','Disbursal Date','Disbursal Amount','Purpose Of Loan','Loan Cycle','Interest Rate(%)','Principal Collected','Interest Collected',
+  'Total Collected','Closure / Rescheduled Date','Reason','Days Since Loan Closed','New Loan  Taken']
+ 
+  /** assorted Labels                  ***/
+  def officeLabel = "Office:"
+  def loanOfficerLabel = "Loan Officer:"
+  def centerLabel = "Center:"
+  def disbursalPeriodLabel = "Disbursal Period:"
+  def closurePeriodLabel = "Closure Period:"
+  def toLabel="To" 
+  def centerGroupHeaderLabel="Office, Loan Officer, Center:"
+  /** *Expected Data              **/
+  def mfiName = 'Mifos HO'
+
+ /**Details Closed obligation met**/
+  def groupName_Closedobligationmet='group dw1 1'
+  def memberName_Closedobligationmet='lose group ..'
+  def loanId_Closedobligationmet='38'
+  def disbursalDate_Closedobligationmet='2010-07-22'
+  def disbursalAmount_Closedobligationmet='3000.0000'
+  def purposeOfLoan_Closedobligationmet=''
+  def loanCycle_Closedobligationmet='1'
+  def interestRate_Closedobligationmet='5.0000000000'
+  def principalCollected_Closedobligationmet='3000.0000'
+  def interestCollected_Closedobligationmet='2.9000'
+  def totalCollected_Closedobligationmet='3002.9000'
+  def closureDate_Closedobligationmet='2010-07-24'
+  def reason_Closedobligationmet='Closed obligation met'
+  def daysSinceLoanClosed_Closedobligationmet='233'
+  def newLoanTaken_Closedobligationmet='No'
+  
+   /**Details Closed Written Off**/
+  def groupName_ClosedWrittenOff='group dw1 2'
+  def memberName_ClosedWrittenOff='client dw1 1 1 ..'
+  def loanId_ClosedWrittenOff='4'
+  def disbursalDate_ClosedWrittenOff='2010-07-16'
+  def disbursalAmount_ClosedWrittenOff='3000.0000'
+  def purposeOfLoan_ClosedWrittenOff=''
+  def loanCycle_ClosedWrittenOff='1'
+  def interestRate_ClosedWrittenOff='5.0000000000'
+  def principalCollected_ClosedWrittenOff='247.1000'
+  def interestCollected_ClosedWrittenOff='2.9000'
+  def totalCollected_ClosedWrittenOff='250.0000'
+  def closureDate_ClosedWrittenOff='2010-07-18'
+  def reason_ClosedWrittenOff='Closed written off'
+  def daysSinceLoanClosed_ClosedWrittenOff='239'
+  def newLoanTaken_ClosedWrittenOff='No'
+  
+  
+  def totalAllBranch_centerdw1_20100101_20110314 = ['Center Total:', 'center dw1', '1', '3000.0000', '3000.0000','2.9000', '3002.9000']
+  def totalAllBranch_br2LOxxx_20100101_20110314 = ['Loan Officer Total:', 'br2 LO xxx', '1', '3000.0000', '3000.0000', '2.9000', '3002.9000']
+  def totalBranch_BO1_20100101_20110314 = ['Branch Total:', 'BO 1', '1', '3000.0000', '3000.0000', '2.9000', '3002.9000']
+ 
+  def totalBO1_centerdw1_20100718_20100720 = ['Center Total:', 'center dw1', '1', '3000.0000', '247.1000','2.9000', '250.0000']
+  def totalBO1_br1lo1smith_20100718_20100720 = ['Loan Officer Total:', 'br1 lo 1 smith', '1', '3,000.0000', '247.1000', '2.9000', '250.0000']
+  def totalBO1_BO1_20100718_20100720 = ['Branch Total:', 'BO 1', '1', '3000.0000', '247.1000', '2.9000', '250.0000']
+  
+
+  def pageFooter = ['Version 1.2', 'Page']
+
+
+  @Test
+  void testParams_BO1_ClosedObligationMet_20100101_20110314() {
+    new PrptReport().execute() { t ->
+
+      // Report settings.
+      t.reportPath = reportPath
+      t.reportParams = ['selected_office': '-1', 'selected_officer': '-1', 'selected_center': '-1', 'selected_status': 'Closed - obligation met', 'start_date': '2010-01-01', 'end_date': '2011-03-14', 'disbursal_or_closed_date': 'true']
+
+      // MFI Name
+      t.assertRowEquals(1, [mfiName])
+      // Report title
+      t.assertRowEquals(2, [reportName])
+
+      // Report header labels and values
+      t.assertCellEquals(3, 1, officeLabel)
+      t.assertCellEquals(3, 2, 'All')
+      t.assertCellEquals(3, 3, 'disbursalPeriodLabel')
+     // TODO t.assertCellEquals(3, 4, '2010-01-01')
+      t.assertCellEquals(3, 5, 'toLabel')
+     //TODO t.assertCellEquals(3, 6, '2011-03-14')
+      t.assertCellEquals(4, 1, loanOfficerLabel)
+      t.assertCellEquals(4, 2, 'All')
+
+      t.assertCellEquals(5, 1, centerLabel)
+      t.assertCellEquals(5, 2, 'All')
+     
+      // Details header
+      def offset = 6
+      t.assertRowEquals(offset, closedLoansHeader)
+      t.assertRowEquals(offset + 1, centerGroupHeaderLabel)
+      t.assertCellEquals(offset + 1,1, 'BO 1, br2 LO xxx, center dw1')
+     
+
+      // Details
+      t.assertCellEquals(offset + 2,1, groupName_Closedobligationmet)
+      t.assertCellEquals(offset + 2,2, memberName_Closedobligationmet)
+      t.assertCellEquals(offset + 2,3, loanId_Closedobligationmet)
+      //TODO t.assertCellEquals(offset + 2,4, disbursalDate_Closedobligationmet)
+      t.assertCellEquals(offset + 2,5, disbursalAmount_Closedobligationmet)
+      t.assertCellEquals(offset + 2,6, purposeOfLoan_Closedobligationmet)
+      t.assertCellEquals(offset + 2,7, loanCycle_Closedobligationmet)
+      t.assertCellEquals(offset + 2,8, interestRate_Closedobligationmet)
+      t.assertCellEquals(offset + 2,9, principalCollected_Closedobligationmet)
+      t.assertCellEquals(offset + 2,10, interestCollected_Closedobligationmet)
+      t.assertCellEquals(offset + 2,11, totalCollected_Closedobligationmet)
+      //TODO t.assertCellEquals(offset + 2,12, closureDate_Closedobligationmet)
+      t.assertCellEquals(offset + 2,13, reason_Closedobligationmet)
+      //TODO t.assertCellEquals(offset + 2,14, daysSinceLoanClosed_Closedobligationmet)
+      t.assertCellEquals(offset + 2,15, newLoanTaken_Closedobligationmet)
+
+      // Group footer
+      t.assertRowEquals(offset + 3, totalAllBranch_centerdw1_20100101_20110314)
+      t.assertRowEquals(offset + 4, totalAllBranch_br2LOxxx_20100101_20110314)
+      t.assertRowEquals(offset + 5, totalBranch_BO1_20100101_20110314)
+
+      // Page footer
+      offset = 12
+      t.assertRowEquals(offset, pageFooter)
+      t.assertCellEquals(offset, 3, '1 / 1')
+      t.assertCellEquals(offset + 1, 1, 'Printed On:')
+
+      
+      t.assertCellEquals(offset + 2, 1, 'Printed by:')
+      // t.assertCellEquals(offset + 2, 2, 'Designer')
+    }
+  }
+
+  @Test
+  void testParams_BO1_ClosedWriteenOff_20100718_20100720() {
+    new PrptReport().execute() { t ->
+
+      // Report settings.
+      t.reportPath = reportPath
+      t.reportParams = ['selected_office': '3', 'selected_officer': '-1', 'selected_center': '-1', 'selected_status': 'Closed - written off', 'start_date': '2010-07-08', 'end_date': '2010-07-20', 'disbursal_or_closed_date': 'false']
+
+      // MFI Name
+      t.assertRowEquals(1, [mfiName])
+      // Report title
+      t.assertRowEquals(2, [reportName])
+
+      // Report header
+      t.assertCellEquals(3, 1, officeLabel)
+      t.assertCellEquals(3, 2, 'BO 1')
+      t.assertCellEquals(4, 1, loanOfficerLabel)
+      t.assertCellEquals(4, 2, 'All')
+      t.assertCellEquals(4, 3, 'closurePeriodLabel')
+      //TODO t.assertCellEquals(4, 4, '2010-07-18')
+      t.assertCellEquals(4, 5, 'toLabel')
+     //TODO  t.assertCellEquals(4, 6, '2011-07-20')
+      t.assertCellEquals(5, 1, centerLabel)
+      t.assertCellEquals(5, 2, 'All')
+     
+
+      // Details header
+      def offset = 6
+      t.assertRowEquals(offset, closedLoansHeader)
+      t.assertRowEquals(offset + 1, centerGroupHeaderLabel)
+      t.assertCellEquals(offset + 1,1, 'BO 1, br1 lo 1 smith, center dw1')
+
+      /** Details **/
+     
+       t.assertCellEquals(offset + 2,1, groupName_ClosedWrittenOff)
+      t.assertCellEquals(offset + 2,2, memberName_ClosedWrittenOff)
+      t.assertCellEquals(offset + 2,3, loanId_ClosedWrittenOff)
+      //TODO t.assertCellEquals(offset + 2,4, disbursalDate_ClosedWrittenOff)
+      t.assertCellEquals(offset + 2,5, disbursalAmount_ClosedWrittenOff)
+      t.assertCellEquals(offset + 2,6, purposeOfLoan_ClosedWrittenOff)
+      t.assertCellEquals(offset + 2,7, loanCycle_ClosedWrittenOff)
+      t.assertCellEquals(offset + 2,8, interestRate_ClosedWrittenOff)
+      t.assertCellEquals(offset + 2,9, principalCollected_ClosedWrittenOff)
+      t.assertCellEquals(offset + 2,10, interestCollected_ClosedWrittenOff)
+      t.assertCellEquals(offset + 2,11, totalCollected_ClosedWrittenOff)
+      //TODO t.assertCellEquals(offset + 2,12, closureDate_ClosedWrittenOff)
+      t.assertCellEquals(offset + 2,13, reason_ClosedWrittenOff)
+      //TODO t.assertCellEquals(offset + 2,14, daysSinceLoanClosed_ClosedWrittenOff)
+      t.assertCellEquals(offset + 2,15, newLoanTaken_ClosedWrittenOff)
+
+      
+     
+
+      // Group footer
+      t.assertRowEquals(offset + 3, totalBO1_centerdw1_20100718_20100720)
+      t.assertRowEquals(offset + 4, totalBO1_br1lo1smith_20100718_20100720)
+      t.assertRowEquals(offset + 5, totalBO1_BO1_20100718_20100720)
+      // Page footer
+      offset = 12
+
+      t.assertRowEquals(offset, pageFooter)
+      t.assertCellEquals(offset, 3, '1 / 1')
+      t.assertCellEquals(offset + 1, 1, 'Printed by:')
+      t.assertCellEquals(offset + 1, 3, 'On:')
+      
+      
+
+    }
+  }
+
+
+
+ 
+
+}
---
+++ reportTests/src/test/groovy/org/mifos/bi/test/OverdueMatureLoans.groovy
@@ -0,0 +1,139 @@
+package org.mifos.bi.test
+
+import org.junit.Test
+
+class OverdueMatureLoans {
+
+
+  def reportPath = 'reports/standardReports/prpts/OverdueMatureLoans.prpt'
+  def reportName = 'Overdue Mature Loans'
+
+  /** Headers for Overdue Mature Loans**/
+  
+  def overdueLoansHeader = ['Group Name','Member Name','Loan Id','Disbursal Date','Disbursal Amount','Purpose Of Loan','Principal Repaid','Interest Repaid','Last Paid Date','Principal Overdue','Interest Overdue','Principal Overdue(%)','Principal Outstanding','Interest Outstanding']
+  def overdueMatureLoansHeader = ['Group Name','Member Name','Loan Id','Disbursal Date','Disbursal Amount','Purpose Of Loan','Principal Repaid','Interest Repaid','Last Paid Date','Principal Overdue','Interest Overdue','Principal Overdue(%)']
+ 
+  /** assorted Labels                  ***/
+  def officeLabel = "Office:"
+  def loanOfficerLabel = "Loan Officer:"
+  def centerLabel = "Center:"
+  def overdueMatureLoansLabel = "Filter By Overdue Mature Loans:"
+  def centerGroupHeaderLabel="Office, Loan Officer, Center:"
+  /** *Expected Data              **/
+  def mfiName = 'Mifos HO'
+
+  def detailsAllBranch_OverdueLoans = ['br2 group 1', 'br2 client 1 xxx', '32', '2010-07-21', '3000.0000', '', '75.1000', '2.9000','2010-07-21','2571.9000','18.1000','85.73','2924.9000','18.1000']
+  def centerTotal_br2center1_OverdueLoans = ['Center Total:', 'br2 center 1', '1', '3000.00', '75.1000','2.9000', '2571.9000','18.1000','2924.9000','18.1000']
+  def loanOfficerTotal_br2LOxxx_OverdueLoans = ['Loan Officer Total:', 'br2 LO xxx', '1', '3000.00', '75.1000','2.9000', '2571.9000','18.1000','2924.9000','18.1000']
+  def branchTotal_br2_OverdueLoans = ['Branch Total:', 'br2', '1', '3000.0000', '75.1000','2.9000', '2571.9000','18.1000','2924.9000','18.1000']
+  def detailsAllBranch_OverdueMatureLoans = ['br2 group 1', 'br2 client 1 xxx', '32', '2010-07-21', '3000.0000', '', '75.1000', '2.9000','2010-07-21','2571.9000','18.1000','85.73']
+  def centerTotal_br2center1_OverdueMatureLoans = ['Center Total:', 'br2 center 1', '1', '3000.00', '75.1000','2.9000', '2571.9000','18.1000']
+  def loanOfficerTotal_br2LOxxx_OverdueMatureLoans = ['Loan Officer Total:', 'br2 LO xxx', '1', '3000.00', '75.1000','2.9000', '2571.9000','18.1000']
+  def branchTotal_br2_OverdueMatureLoans = ['Branch Total:', 'br2', '1', '3000.00', '75.1000','2.9000', '2571.9000','18.1000']
+  
+
+  def pageFooter = ['Version 1.2', 'Page']
+
+
+  @Test
+  void testParams_br2_OverdueLoans() {
+    new PrptReport().execute() { t ->
+
+      // Report settings.
+      t.reportPath = reportPath
+      t.reportParams = ['selected_office': '-1', 'selected_officer': '-1', 'selected_center': '-1', 'selected_loans': 'false']
+
+      // MFI Name
+      t.assertRowEquals(1, [mfiName])
+      // Report title
+      t.assertRowEquals(2, [reportName])
+
+      // Report header labels and values
+      t.assertCellEquals(3, 1, officeLabel)
+      t.assertCellEquals(3, 2, 'All')
+      t.assertCellEquals(3, 3, 'overdueMatureLoansLabel')
+      t.assertCellEquals(3, 4, 'No')
+      t.assertCellEquals(4, 1, loanOfficerLabel)
+      t.assertCellEquals(4, 2, 'All')
+
+      t.assertCellEquals(5, 1, centerLabel)
+      t.assertCellEquals(5, 2, 'All')
+     
+      // Details header
+      def offset = 6
+      t.assertRowEquals(offset, overdueMatureLoansHeader)
+      t.assertRowEquals(offset + 1, centerGroupHeaderLabel)
+      t.assertCellEquals(offset + 1,1, 'br2, br2 LO xxx, br2 center 1')
+     
+
+      // Details
+      t.assertRowEquals(offset + 2, detailsAllBranch_OverdueLoans)
+
+      // Group footer
+      t.assertRowEquals(offset + 3, centerTotal_br2center1_OverdueLoans)
+      t.assertRowEquals(offset + 4, loanOfficerTotal_br2LOxxx_OverdueLoans)
+      t.assertRowEquals(offset + 5, branchTotal_br2_OverdueLoans)
+
+      // Page footer
+      offset = 12
+      t.assertRowEquals(offset, pageFooter)
+      t.assertCellEquals(offset, 3, '1 / 1')
+      t.assertCellEquals(offset + 1, 1, 'Printed by:')
+
+      
+      t.assertCellEquals(offset + 1, 3, 'On:')
+     
+    }
+  }
+
+  @Test
+  void testParams_br2_OverdueMatureLoans() {
+    new PrptReport().execute() { t ->
+
+      // Report settings.
+      t.reportPath = reportPath
+      t.reportParams = ['selected_office': '-1', 'selected_officer': '-1', 'selected_center': '-1', 'selected_loans': 'true']
+
+      // MFI Name
+      t.assertRowEquals(1, [mfiName])
+      // Report title
+      t.assertRowEquals(2, [reportName])
+
+      // Report header labels and values
+      t.assertCellEquals(3, 1, officeLabel)
+      t.assertCellEquals(3, 2, 'All')
+      t.assertCellEquals(3, 3, 'overdueMatureLoansLabel')
+      t.assertCellEquals(3, 4, 'Yes')
+      t.assertCellEquals(4, 1, loanOfficerLabel)
+      t.assertCellEquals(4, 2, 'All')
+
+      t.assertCellEquals(5, 1, centerLabel)
+      t.assertCellEquals(5, 2, 'All')
+     
+      // Details header
+      def offset = 6
+      t.assertRowEquals(offset, overdueLoansHeader)
+      t.assertRowEquals(offset + 1, centerGroupHeaderLabel)
+      t.assertCellEquals(offset + 1,1, 'br2, br2 LO xxx, br2 center 1')
+     
+
+      // Details
+      t.assertRowEquals(offset + 2, detailsAllBranch_OverdueMatureLoans)
+
+      // Group footer
+      t.assertRowEquals(offset + 3, centerTotal_br2center1_OverdueMatureLoans)
+      t.assertRowEquals(offset + 4, loanOfficerTotal_br2LOxxx_OverdueMatureLoans)
+      t.assertRowEquals(offset + 5, branchTotal_br2_OverdueMatureLoans)
+
+      // Page footer
+      offset = 12
+      t.assertRowEquals(offset, pageFooter)
+      t.assertCellEquals(offset, 3, '1 / 1')
+      t.assertCellEquals(offset + 1, 1, 'Printed by:')
+
+      
+      t.assertCellEquals(offset + 1, 3, 'On:')
+     
+    }
+  }
+  }
