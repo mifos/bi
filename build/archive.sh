46cdd35089fa6a4e1a4b7981fcf4a427852a20ad
Sumit Shah <sshah@grameenfoundation.org>
2011-04-19 17:15:33 -0700

    fix archive script

--- build/archive.sh
+++ build/archive.sh
@@ -8,6 +8,6 @@ set -e
 PREFIX=mifos_bi-1.2.0
 
 mkdir -p ${PREFIX}/ETL
-cp -r BUILD_ENV.txt README INSTALL LICENSE ETL/MifosDataWarehouseETL reports ${PREFIX}/
+cp -r BUILD_ENV.txt README INSTALL LICENSE reports ${PREFIX}/
 cp -r ETL/MifosDataWarehouseETL ${PREFIX}/ETL
 /usr/bin/zip -r ${PREFIX}.zip ${PREFIX}
