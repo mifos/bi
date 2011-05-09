f4edee6256df51991f8f7a6c8a28b9b884827e1e
John Woodlock <john.woodlock@gmail.com>
2011-05-09 21:06:51 +1000

    MIFOSBI-47 - Create ETL Job that updates DW incrementally - WIP

--- etl_build_prod.sh
+++ etl_build_prod.sh
@@ -25,6 +25,8 @@ echo "drop database $DW_DB_NAME" | mysql $MYSQL_ARGS
 echo "create database $DW_DB_NAME" | mysql $MYSQL_ARGS
 mysql $MYSQL_ARGS $DW_DB_NAME < $PRGDIR/ETL/MifosDataWarehouseETL/load_mifos_datawarehouse.sql
 mysql $MYSQL_ARGS $DW_DB_NAME < $PRGDIR/ETL/MifosDataWarehouseETL/load_mifos_datawarehouse_stored_procedures.sql
+mysql $MYSQL_ARGS $DW_DB_NAME < $PRGDIR/ETL/MifosDataWarehouseETL/load_ppi_poverty_lines.sql
+mysql $MYSQL_ARGS $DW_DB_NAME < $PRGDIR/ETL/MifosDataWarehouseETL/load_dw_ppi_survey.sql
 
 echo "Running ETL..."
 log=`mktemp`
