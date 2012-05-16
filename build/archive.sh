ada8ad4336901f81141c0f0c95aace2f992a83d2
Jakub Slawinski <jeremian@poczta.fm>
2012-05-16 14:34:08 +0200

    Removed misc reports/data access from official standard release.

--- build/archive.sh
+++ build/archive.sh
@@ -9,6 +9,8 @@ PREFIX=mifos_bi-1.3.1
 
 mkdir -p ${PREFIX}/ETL
 cp -r BUILD_ENV.txt README INSTALL LICENSE reports DashboardsTemplate ${PREFIX}/
+rm -rf ${PREFIX}/reports/MiscdataAccess
+rm -rf ${PREFIX}/reports/MiscReports
 cp -r Dashboards ${PREFIX}/reports
 cp -r DashboardsMenu ${PREFIX}/reports
 cp -r ETL/MifosDataWarehouseETL ${PREFIX}/ETL
